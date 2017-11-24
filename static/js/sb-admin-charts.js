422db8061494f2cda358f147f156e42754d8d0b4
danjrary <danjrary@gmail.com>
2017-11-24 18:20:40 +0800

    add hightlight area, environmental chart, News feed area

--- static/js/sb-admin-charts.js
+++ static/js/sb-admin-charts.js
@@ -57,13 +57,27 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["2012", "2013", "2014", "2015", "2016", "2017"],
     datasets: [{
-      label: "Revenue",
+      label: "範疇一",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
+      data: [815, 912, 1051, 1341, 2021, 2284],
+    },
+    {
+      label: "範疇二",
+      backgroundColor: "rgba(185,185,185,1)",
+      borderColor: "rgba(2,117,216,1)",
       data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
+    },
+    {
+      label: "範疇三",
+      backgroundColor: "rgba(85,85,85,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [215, 312, 251, 841, 821, 1184],
+    }
+
+    ],
   },
   options: {
     scales: {
@@ -76,22 +90,24 @@ var myLineChart = new Chart(ctx, {
         },
         ticks: {
           maxTicksLimit: 6
-        }
+        },
+        stacked: true
       }],
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 20000,
           maxTicksLimit: 5
         },
         gridLines: {
           display: true
-        }
+        },
+        stacked: true
       }],
     },
     legend: {
       display: false
-    }
+    },
   }
 });
 // -- Pie Chart Example
@@ -99,10 +115,10 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
+    labels: ["外購電力", "液化石油氣", "柴油", "再生能源"],
     datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+      data: [19456776, 4756476, 919160, 591100],
+      backgroundColor: ['#007bff', '#dc3545', '#585859', '#28a745'],
     }],
   },
 });
@@ -113,8 +129,9 @@ var ctx = document.getElementById("PieChart_score");
 var PieChart_score = new Chart(ctx, {
   type: 'doughnut',
   data: {
+    lables: ["", ""],
     datasets: [{
-      data: [83, 17],
+      data: [80, 20],
       backgroundColor: ['#4285F4', '#D8D8D8'],
     }],
   },
