236d7c9e5d0de4e77184adb984ebda1d399887c6
danjrary <danjrary@gmail.com>
2017-12-05 18:37:42 +0800

    success upload data.csv to heroku and try upload sdg.csv

---
+++ import_sdg2.py
@@ -0,0 +1,23 @@
+import csv,sys,os
+
+project_dir = "/firstproject/"
+sys.path.append(project_dir)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'firstproject.settings'
+
+import django
+
+django.setup()
+
+from myapp.models import sdgs
+
+data = csv.reader(open("./sdg.csv"),delimiter=",")
+
+for row in data:
+	if row[0] != 'sdg_Name':
+		unit = sdgs()
+		unit.sdg_Name = row[0]
+		unit.sdg_Num = row[1]
+		unit.sdg_Goal = row[2]
+		unit.sdg_img_url = row[3]
+		unit.save()
